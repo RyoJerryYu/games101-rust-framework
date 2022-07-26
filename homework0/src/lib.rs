8aac137d2ce20ebb97bffde260cff49d348b1e21
Ryo <tzl19961216@gmail.com>
2022-07-26 22:31:16 +0800

    impl homework 0

--- homework0/src/lib.rs
+++ homework0/src/lib.rs
@@ -1,5 +1,10 @@
-use glam::Vec3;
+use std::f32::consts::PI;
+
+use glam::{Vec3, Mat3, Vec2};
 
 pub fn your_code() -> Vec3 {
-    unimplemented!();
+    let p = Vec3::new(2.0, 1.0, 1.0);
+    let m1 = Mat3::from_angle(PI*45.0/180.0);
+    let m2 = Mat3::from_translation(Vec2{x: 1.0, y: 2.0});
+    m2 * m1 * p
 }
