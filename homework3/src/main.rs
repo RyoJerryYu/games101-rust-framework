575d855777dabd2d733c2e56dcf1bb8123595a8a
Ryo <tzl19961216@gmail.com>
2022-12-30 02:35:25 +0800

    feat: add obj-rs as obj loader

--- homework3/src/main.rs
+++ homework3/src/main.rs
@@ -1,6 +1,10 @@
+use obj::load_obj;
 use utils::triangle::{Triangle, Rgb};
 use anyhow::Result;
 
 fn main() -> Result<()> {
+    let input = std::io::BufReader::new(std::fs::File::open("input.obj")?);
+    let model: obj::Obj<obj::TexturedVertex, u8> = load_obj(input)?;
+    model.vertices.iter().for_each(|v| println!("{:?}", v));
     return  Ok(());
 }
--- homework3/src/shader.rs
+++ homework3/src/shader.rs
@@ -1,33 +1,6 @@
 use anyhow::Result;
 use glam::{Vec2, Vec3};
 
-/*
-class Texture{
-private:
-    cv::Mat image_data;
-
-public:
-    Texture(const std::string& name)
-    {
-        image_data = cv::imread(name);
-        cv::cvtColor(image_data, image_data, cv::COLOR_RGB2BGR);
-        width = image_data.cols;
-        height = image_data.rows;
-    }
-
-    int width, height;
-
-    Eigen::Vector3f getColor(float u, float v)
-    {
-        auto u_img = u * width;
-        auto v_img = (1 - v) * height;
-        auto color = image_data.at<cv::Vec3b>(v_img, u_img);
-        return Eigen::Vector3f(color[0], color[1], color[2]);
-    }
-
-};
- */
-
 pub struct Texture {
     pub width: u32,
     pub height: u32,
