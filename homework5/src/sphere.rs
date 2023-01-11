beb1cd82e67ea4d321accf4d651e64ca7b023d9f
Ryo <tzl19961216@gmail.com>
2023-01-12 00:24:55 +0800

    feat: impl sphere and triangle for hw5

--- homework5/src/sphere.rs
+++ homework5/src/sphere.rs
@@ -2,6 +2,33 @@ use glam::Vec3;
 
 use crate::object::{MaterialType, Object, ObjectRenderPayload};
 
+// return x0 < x1
+#[inline]
+fn solve_quadratic(a: f32, b: f32, c: f32, x0: &mut f32, x1: &mut f32) -> bool {
+    let discr = b * b - 4.0 * a * c;
+    if discr < 0.0 {
+        return false;
+    }
+    if discr == 0.0 {
+        *x0 = -0.5 * b / a;
+        *x1 = *x0;
+    } else {
+        let q = if b > 0.0 {
+            -0.5 * (b + discr.sqrt())
+        } else {
+            -0.5 * (b - discr.sqrt())
+        };
+        *x0 = q / a;
+        *x1 = c / q;
+    }
+
+    if x0 > x1 {
+        (*x0, *x1) = (*x1, *x0);
+    }
+
+    return true;
+}
+
 pub struct Sphere {
     center: Vec3,
     radius: f32,
@@ -9,33 +36,56 @@ pub struct Sphere {
 }
 
 impl Object for Sphere {
+    // intersect only when (p - center) ^ 2 = radius ^ 2
+    // where p = orig + t * dir
+    // => (orig - center)^2 - radius^2 - 2*dir.dot(orig-center)t + dir^2*t^2 = 0
+    // => a = dir^2
+    //    b = 2*dir.dot(orig-center)
+    //    c = (orig-center)^2 - radius^2
     fn intersect(
         &self,
         orig: &glam::Vec3,
         dir: &glam::Vec3,
-        tnear: &f32,
-        index: &mut u32,
+        tnear: &mut f32,
+        index: &mut usize,
         uv: &mut glam::Vec2,
     ) -> bool {
-        todo!()
+        // analytic solution
+        let l = *orig - self.center;
+        let a = dir.dot(*dir);
+        let b = 2.0 * dir.dot(l);
+        let c = l.dot(l) - self.radius * self.radius;
+
+        let (mut t0, mut t1) = (0.0, 0.0);
+        if !solve_quadratic(a, b, c, &mut t0, &mut t1) {
+            return false;
+        }
+        if t0 < 0.0 {
+            t0 = t1;
+        }
+        if t0 < 0.0 {
+            return false;
+        }
+
+        *tnear = t0;
+        return true;
     }
 
     fn get_surface_properties(
         &self,
         p: &glam::Vec3,
         pp: &glam::Vec3,
-        index: &u32,
+        index: &usize,
         uv: &glam::Vec2,
         n: &mut glam::Vec3,
         st: &mut glam::Vec2,
     ) {
-        todo!()
+        *n = (*p - self.center).normalize();
     }
 
     fn get_render_payload(&self) -> &ObjectRenderPayload {
         &self.render_payload
     }
-
 }
 
 impl Sphere {
@@ -44,6 +94,111 @@ impl Sphere {
             center: *c,
             radius: r,
             render_payload: ObjectRenderPayload::new(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use glam::Vec2;
+
+    use super::*;
+
+    fn assert_f32_eq(a: f32, b: f32) {
+        assert!((a - b).abs() < 0.00001, "a={} b={}", a, b)
+    }
+
+    #[test]
+    fn test_solve_quadratic() {
+        struct TestCase {
+            abc: (f32, f32, f32),
+            want: (f32, f32),
+        }
+
+        impl TestCase {
+            fn a(&self) -> f32 {
+                self.abc.0
+            }
+            fn b(&self) -> f32 {
+                self.abc.1
+            }
+            fn c(&self) -> f32 {
+                self.abc.2
+            }
+        }
+
+        let test_cases = [
+            TestCase {
+                abc: (1.0, 0.0, -1.0),
+                want: (-1.0, 1.0),
+            },
+            TestCase {
+                abc: (1.0, 2.0, 1.0),
+                want: (-1.0, -1.0),
+            },
+            TestCase {
+                abc: (1.0, -2.0, 1.0),
+                want: (1.0, 1.0),
+            },
+            TestCase {
+                abc: (-1.0, 2.0, -1.0),
+                want: (1.0, 1.0),
+            },
+            TestCase {
+                abc: (1.0, -5.0, 6.0),
+                want: (2.0, 3.0),
+            },
+        ];
+
+        for tc in test_cases {
+            let (mut x0, mut x1) = (0.0, 0.0);
+            let ok = solve_quadratic(tc.a(), tc.b(), tc.c(), &mut x0, &mut x1);
+            assert!(ok);
+            assert_f32_eq(tc.want.0, x0);
+            assert_f32_eq(tc.want.1, x1);
+        }
+    }
+
+    #[test]
+    fn test_intersect() {
+        let sphere = Sphere::new(&Vec3::ONE, 1.0);
+
+        struct TestCase {
+            orig: Vec3,
+            dir: Vec3,
+            is_intersect: bool,
+            tnear: f32,
+        }
+
+        let cases = [
+            TestCase {
+                orig: Vec3::ZERO,
+                dir: Vec3::ONE,
+                is_intersect: true,
+                tnear: (1.0 - 3.0f32.sqrt().recip()),
+            },
+            TestCase {
+                orig: Vec3::ZERO,
+                dir: -Vec3::ONE,
+                is_intersect: false,
+                tnear: 0.0,
+            },
+            TestCase {
+                orig: Vec3 { x: -1.0, y: 1.0, z: 1.0 },
+                dir: Vec3::X,
+                is_intersect: true,
+                tnear: 1.0,
+            }
+        ];
+
+        for tc in cases {
+            let mut tnear = 0.0;
+            let (mut index, mut uv) = (0, Vec2::ZERO);
+            let is_intersect = sphere.intersect(&tc.orig, &tc.dir, &mut tnear, &mut index, &mut uv);
+            assert_eq!(is_intersect, tc.is_intersect);
+            if tc.is_intersect {
+                assert_f32_eq(tc.tnear, tnear);
+            }
         }
     }
 }
--- homework5/src/triangle.rs
+++ homework5/src/triangle.rs
@@ -2,10 +2,27 @@ use glam::{Vec2, Vec3};
 
 use crate::object::{Object, ObjectRenderPayload};
 
+fn ray_triangle_intersect(
+    v0: &Vec3,
+    v1: &Vec3,
+    v2: &Vec3,
+    orig: &Vec3,
+    dir: &Vec3,
+    tnear: &mut f32,
+    u: &mut f32,
+    v: &mut f32,
+) -> bool {
+    // TODO: Implement this function that tests whether the triangle
+    // that's specified bt v0, v1 and v2 intersects with the ray (whose
+    // origin is *orig* and direction is *dir*)
+    // Also don't forget to update tnear, u and v.
+    todo!()
+}
+
 pub struct MeshTriangle {
     vertices: Vec<Vec3>,
-    num_triangles: u32,
-    vertex_index: Vec<u32>,
+    num_triangles: usize,
+    vertex_index: Vec<usize>,
     st_coordinates: Vec<Vec2>,
     pub render_payload: ObjectRenderPayload,
 }
@@ -15,32 +32,82 @@ impl Object for MeshTriangle {
         &self,
         orig: &glam::Vec3,
         dir: &glam::Vec3,
-        tnear: &f32,
-        index: &mut u32,
+        tnear: &mut f32,
+        index: &mut usize,
         uv: &mut glam::Vec2,
     ) -> bool {
-        todo!()
+        let mut intersect = false;
+        for k in 0..self.num_triangles {
+            let verts = (0..3)
+                .map(|i| self.vertex_index[k * 3 + i])
+                .map(|vi| self.vertices[vi])
+                .collect::<Vec<Vec3>>();
+            assert!(verts.len() == 3);
+
+            let (mut t, mut u, mut v) = (0.0, 0.0, 0.0);
+
+            if ray_triangle_intersect(
+                &verts[0], &verts[1], &verts[2], orig, dir, &mut t, &mut u, &mut v,
+            ) && t < *tnear
+            {
+                *tnear = t;
+                uv.x = u;
+                uv.y = v;
+                *index = k;
+                intersect |= true;
+            }
+        }
+
+        return intersect;
     }
 
     fn get_surface_properties(
         &self,
         p: &glam::Vec3,
         pp: &glam::Vec3,
-        index: &u32,
+        index: &usize,
         uv: &glam::Vec2,
         n: &mut glam::Vec3,
         st: &mut glam::Vec2,
     ) {
-        todo!()
+        let verts = (0..3)
+            .map(|i| self.vertex_index[index * 3 + i])
+            .map(|vi| self.vertices[vi])
+            .collect::<Vec<Vec3>>();
+        assert!(verts.len() == 3);
+
+        let e0 = (verts[1] - verts[2]).normalize();
+        let e1 = (verts[2] - verts[1]).normalize();
+        *n = e0.cross(e1);
+
+        let sts = (0..3)
+            .map(|i| self.vertex_index[index * 3 + i])
+            .map(|vi| self.st_coordinates[vi])
+            .collect::<Vec<Vec2>>();
+        assert!(sts.len() == 3);
+
+        *st = sts[0] * (1.0 - uv.x - uv.y) + sts[1] * uv.x + sts[2] * uv.y;
     }
 
     fn get_render_payload(&self) -> &ObjectRenderPayload {
         &self.render_payload
     }
+
+    fn eval_diffuse_color(&self, st: &Vec2) -> Vec3 {
+        let scale = 5.0;
+        let (w, h) = (st.x * scale, st.y * scale);
+        let pattern = ((w - w.floor()) > 0.5) ^ ((h - h.floor()) > 0.5);
+        if pattern {
+            Vec3::new(0.937, 0.937, 0.231)
+        }else {
+            Vec3::new(0.815, 0.235, 0.031)
+        }
+
+    }
 }
 
 impl MeshTriangle {
-    pub fn new(verts: Vec<Vec3>, verts_index: Vec<u32>, num_tris: u32, st: Vec<Vec2>) -> Self {
+    pub fn new(verts: Vec<Vec3>, verts_index: Vec<usize>, num_tris: usize, st: Vec<Vec2>) -> Self {
         Self {
             vertices: verts,
             vertex_index: verts_index,
