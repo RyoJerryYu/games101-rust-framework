428ac7b751d6121589fccf96d4b6ba456393fa99
Ryo <tzl19961216@gmail.com>
2023-01-12 23:25:46 +0800

    feat: impl triangle intersect for hw5

--- homework5/src/main.rs
+++ homework5/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let mut scene = scene::Scene::new(1280, 960);
 
     let mut sph1 = sphere::Sphere::new(&Vec3::new(-1.0, 0.0, -12.0), 2.0);
+    // sph1.render_payload.material_type = MaterialType::DiffuseAndGlossy;
     sph1.render_payload.material_type = MaterialType::DiffuseAndGlossy;
     sph1.render_payload.diffuse_color = Vec3::new(0.6, 0.7, 0.8);
 
@@ -16,22 +17,22 @@ fn main() {
     scene.add_object(Box::new(sph2));
 
     // add mesh triangle
-    // let verts = vec![
-    //     Vec3::new(-5.0, -3.0, -6.0),
-    //     Vec3::new(5.0, -3.0, -6.0),
-    //     Vec3::new(5.0, -3.0, -16.0),
-    //     Vec3::new(-5.0, -3.0, -16.0),
-    // ];
-    // let vert_index = vec![0, 1, 3, 1, 2, 3];
-    // let st = vec![
-    //     Vec2::new(0.0, 0.0),
-    //     Vec2::new(1.0, 0.0),
-    //     Vec2::new(1.0, 1.0),
-    //     Vec2::new(0.0, 1.0),
-    // ];
-    // let mut mesh = triangle::MeshTriangle::new(verts, vert_index, 2, st);
-    // mesh.render_payload.material_type = MaterialType::DiffuseAndGlossy;
-    // scene.add_object(Box::new(mesh));
+    let verts = vec![
+        Vec3::new(-5.0, -3.0, -6.0),
+        Vec3::new(5.0, -3.0, -6.0),
+        Vec3::new(5.0, -3.0, -16.0),
+        Vec3::new(-5.0, -3.0, -16.0),
+    ];
+    let vert_index = vec![0, 1, 3, 1, 2, 3];
+    let st = vec![
+        Vec2::new(0.0, 0.0),
+        Vec2::new(1.0, 0.0),
+        Vec2::new(1.0, 1.0),
+        Vec2::new(0.0, 1.0),
+    ];
+    let mut mesh = triangle::MeshTriangle::new(verts, vert_index, 2, st);
+    mesh.render_payload.material_type = MaterialType::DiffuseAndGlossy;
+    scene.add_object(Box::new(mesh));
 
     scene.add_light(Box::new(light::PointLight::new(
         &Vec3::new(-20.0, 70.0, 20.0),
