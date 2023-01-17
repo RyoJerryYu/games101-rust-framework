7b747937ab413157960eb55be46281ce90fb48d3
Ryo <tzl19961216@gmail.com>
2023-01-18 00:56:54 +0800

    feat: impl build bvh

--- homework6/src/bounds3.rs
+++ homework6/src/bounds3.rs
@@ -6,14 +6,63 @@ pub struct Bounds3 {
     pub max: Vec3,
 }
 
+pub enum Dimension {
+    X,
+    Y,
+    Z,
+}
+
 impl Bounds3 {
-    pub fn new(min: Vec3, max: Vec3) -> Self {
+    pub fn new() -> Self {
+        Self {
+            min: Vec3::new(f32::MAX, f32::MAX, f32::MAX),
+            max: Vec3::new(f32::MIN, f32::MIN, f32::MIN),
+        }
+    }
+
+    pub fn from_point(p: Vec3) -> Self {
+        Self { min: p, max: p }
+    }
+
+    pub fn from_min_max(min: Vec3, max: Vec3) -> Self {
         let (min, max) = (min.min(max), min.max(max));
         Self { min, max }
     }
 
     pub fn union(&self, other: &Bounds3) -> Bounds3 {
-        todo!()
+        Self {
+            min: self.min.min(other.min),
+            max: self.max.max(other.max),
+        }
+    }
+
+    pub fn union_point(&self, p: Vec3) -> Bounds3 {
+        Self {
+            min: self.min.min(p),
+            max: self.max.max(p),
+        }
+    }
+
+    // the diagonal of the bounding box
+    fn diagonal(&self) -> Vec3 {
+        self.max - self.min
+    }
+
+    // the longest extented dimension
+    pub fn max_extent(&self) -> Dimension {
+        let d = self.diagonal();
+        if d.x > d.y && d.x > d.z {
+            Dimension::X
+        } else if d.y > d.z {
+            Dimension::Y
+        } else {
+            Dimension::Z
+        }
+    }
+
+    // the center of the bounding box
+    pub fn centroid(&self) -> Vec3 {
+        (self.min + self.max) / 2.0
     }
 
     pub fn intersect(&self, ray: &crate::ray::Ray) -> bool {
--- homework6/src/bvh.rs
+++ homework6/src/bvh.rs
@@ -1,7 +1,7 @@
 use glam::Vec3;
 
 use crate::{
-    bounds3::Bounds3,
+    bounds3::{Bounds3, Dimension},
     object::{intersection::Intersection, object::Object},
     ray::Ray,
 };
@@ -21,7 +21,6 @@ struct BVHBuildNode {
 pub struct BVHAccel {
     maxPrimsInNode: usize,
     splitMethod: BVHSplitMethod,
-    primitives: Vec<Box<dyn Object>>,
     root: Option<Box<BVHBuildNode>>,
 }
 
@@ -30,23 +29,95 @@ impl BVHAccel {
         let mut res = Self {
             maxPrimsInNode: 1,
             splitMethod: BVHSplitMethod::NAIVE,
-            primitives: p,
             root: None,
         };
-        if res.primitives.len() == 0 {
+        if p.len() == 0 {
             return res;
         }
 
         // start time
-        res.root = Some(Box::new(BVHAccel::recursive_build(&res.primitives)));
+        res.root = Some(Box::new(BVHAccel::recursive_build(p)));
         // end time
         // logging...
 
         return res;
     }
 
-    fn recursive_build(objects: &Vec<Box<dyn Object>>) -> BVHBuildNode {
-        todo!()
+    fn recursive_build(objects: Vec<Box<dyn Object>>) -> BVHBuildNode {
+        if objects.len() == 0 {
+            panic!("logic error")
+        }
+
+        match objects.len() {
+            1 => {
+                return BVHBuildNode {
+                    bounds: objects[0].get_bounds().clone(),
+                    left: None,
+                    right: None,
+                    object: objects.into_iter().nth(0),
+                };
+            }
+            2 => {
+                let mut left_objs = objects;
+                let right_objs = left_objs.split_off(1);
+
+                let left = BVHAccel::recursive_build(left_objs);
+                let right = BVHAccel::recursive_build(right_objs);
+
+                return BVHBuildNode {
+                    bounds: left.bounds.union(&right.bounds),
+                    left: Some(Box::new(left)),
+                    right: Some(Box::new(right)),
+                    object: None,
+                };
+            }
+            _ => {
+                let mut centroid_bounds = Bounds3::new();
+                for object in objects.iter() {
+                    centroid_bounds = centroid_bounds.union_point(object.get_bounds().centroid());
+                }
+
+                let mut objects = objects;
+                match centroid_bounds.max_extent() {
+                    Dimension::X => objects.sort_by(|a, b| {
+                        a.get_bounds()
+                            .centroid()
+                            .x
+                            .partial_cmp(&b.get_bounds().centroid().x)
+                            .unwrap()
+                    }),
+                    Dimension::Y => objects.sort_by(|a, b| {
+                        a.get_bounds()
+                            .centroid()
+                            .y
+                            .partial_cmp(&b.get_bounds().centroid().y)
+                            .unwrap()
+                    }),
+                    Dimension::Z => objects.sort_by(|a, b| {
+                        a.get_bounds()
+                            .centroid()
+                            .z
+                            .partial_cmp(&b.get_bounds().centroid().z)
+                            .unwrap()
+                    }),
+                }
+
+                let mid = objects.len() / 2;
+
+                let mut left_objs = objects;
+                let right_objs = left_objs.split_off(mid);
+
+                let left = BVHAccel::recursive_build(left_objs);
+                let right = BVHAccel::recursive_build(right_objs);
+
+                return BVHBuildNode {
+                    bounds: left.bounds.union(&right.bounds),
+                    left: Some(Box::new(left)),
+                    right: Some(Box::new(right)),
+                    object: None,
+                };
+            }
+        }
     }
 
     pub fn intersect(&self, ray: &Ray) -> Option<Intersection> {
