86f9f99074a2db9d740457a041385e83ec43c93e
Ryo <tzl19961216@gmail.com>
2023-01-18 17:24:56 +0800

    feat: improve intersection for using reference

--- homework6/src/object/intersection.rs
+++ homework6/src/object/intersection.rs
@@ -2,24 +2,10 @@ use glam::Vec3;
 
 use crate::object::{material::Material, object::Object};
 
-pub struct Intersection {
+pub struct Intersection<'a> {
     pub coords: Vec3,
     pub normal: Vec3,
     pub distance: f32,
-    pub obj: Box<dyn Object>,
-    pub m: Box<Material>,
-}
-
-
-impl Intersection {
-    // pub fn new() -> Self {
-    //     Self {
-    //         happended: false,
-    //         coords: Vec3::ZERO,
-    //         normal: Vec3::ZERO,
-    //         distance: 0.0,
-    //         obj: None,
-    //         m: None,
-    //     }
-    // }
+    pub obj: &'a dyn Object,
+    pub m: &'a Material,
 }
