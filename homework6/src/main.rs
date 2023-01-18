7921271bded054344f923ae3f653c732f4d67845
Ryo <tzl19961216@gmail.com>
2023-01-18 21:47:46 +0800

    fix: fix loading bunny model which have no texture or normals

--- homework6/src/main.rs
+++ homework6/src/main.rs
@@ -5,14 +5,15 @@ fn main() {
     let mut scene = Scene::new(1280, 960);
     let mut object_holder = scene.new_object_holder();
 
-    let bunny = MeshTriangle::new("bunny.obj").expect("load bunny.obj failed");
+    let bunny =
+        MeshTriangle::new("homework3/models/bunny/bunny.obj").expect("load bunny.obj failed");
 
     object_holder.add_object(Box::new(bunny));
     scene.add_light(Box::new(PointLight::new(
-        &Vec3::new(-20.0, 70.0, 70.0),
+        &Vec3::new(-20.0, 70.0, 20.0),
         1.0,
     )));
-    scene.add_light(Box::new(PointLight::new(&Vec3::new(20.0, 70.0, 70.0), 1.0)));
+    scene.add_light(Box::new(PointLight::new(&Vec3::new(20.0, 70.0, 20.0), 1.0)));
 
     scene.build_bvh(object_holder);
 
