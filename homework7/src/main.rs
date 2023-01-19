60f351a8cbb3ef9b747ef28e3bf3d7e1a229e1df
Ryo <tzl19961216@gmail.com>
2023-01-19 12:21:30 +0800

    feat: impl triangle materials and sample method

--- homework7/src/main.rs
+++ homework7/src/main.rs
@@ -10,17 +10,35 @@ fn main() -> Result<()> {
     let mut scene = Scene::new(784, 784);
     let mut object_holder = scene.new_object_holder();
 
-    // let mut red = Material::new(MaterialType::DiffuseAndGlossy, color, emission);
-    // red.kd =
-    // let green
-    // let white
+    let red = Material::new(
+        MaterialType::Diffuse,
+        Vec3::ZERO,
+        Vec3::new(0.63, 0.065, 0.05),
+    );
+    let green = Material::new(
+        MaterialType::Diffuse,
+        Vec3::ZERO,
+        Vec3::new(0.14, 0.45, 0.091),
+    );
+    let white = Material::new(
+        MaterialType::Diffuse,
+        Vec3::ZERO,
+        Vec3::new(0.725, 0.71, 0.68),
+    );
+    let light = Material::new(
+        MaterialType::Diffuse,
+        8.0 * Vec3::new(0.747 + 0.058, 0.747 + 0.258, 0.747)
+            + 15.6 * Vec3::new(0.740 + 0.287, 0.740 + 0.160, 0.740)
+            + 18.4 * Vec3::new(0.737 + 0.642, 0.737 + 0.159, 0.737),
+        Vec3::ONE * 0.65,
+    );
     // let liight ...?
 
-    let floor = MeshTriangle::new("homework7/models/cornellbox/floor.obj")?;
-    let shortbox = MeshTriangle::new("homework7/models/cornellbox/shortbox.obj")?;
-    let tallbox = MeshTriangle::new("homework7/models/cornellbox/tallbox.obj")?;
-    let left = MeshTriangle::new("homework7/models/cornellbox/left.obj")?;
-    let right = MeshTriangle::new("homework7/models/cornellbox/right.obj")?;
+    let floor = MeshTriangle::new("homework7/models/cornellbox/floor.obj", &white)?;
+    let shortbox = MeshTriangle::new("homework7/models/cornellbox/shortbox.obj", &white)?;
+    let tallbox = MeshTriangle::new("homework7/models/cornellbox/tallbox.obj", &white)?;
+    let left = MeshTriangle::new("homework7/models/cornellbox/left.obj", &red)?;
+    let right = MeshTriangle::new("homework7/models/cornellbox/right.obj", &green)?;
     // let light
 
     object_holder.add_object(Box::new(floor));
