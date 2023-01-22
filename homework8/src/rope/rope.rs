f5050215b2282dbc117e1fee99b1cca8b894cbb0
Ryo <tzl19961216@gmail.com>
2023-01-23 01:38:07 +0800

    feat: impl semi-implicit euler simulate

--- homework8/src/rope/rope.rs
+++ homework8/src/rope/rope.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, rc::Rc};
+use std::{collections::HashSet, iter::Map, rc::Rc};
 
 use glam::Vec2;
 
@@ -7,6 +7,8 @@ use super::types::{Mass, Spring};
 pub struct Rope {
     pub masses: Vec<Rc<Mass>>,
     pub springs: Vec<Spring>,
+
+    masses_position: Vec<Vec2>,
 }
 
 impl Rope {
@@ -18,6 +20,12 @@ impl Rope {
         k: f32,
         pinned_nodes: Vec<usize>,
     ) -> Self {
+        // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+
+        //        Comment-in this part when you implement the constructor
+        //        for (auto &i : pinned_nodes) {
+        //            masses[i]->pinned = true;
+        //        }
         assert!(num_nodes > 1, "rope less than 2 nodes is nonsense");
         for pinned in &pinned_nodes {
             assert!(pinned <= &num_nodes, "the {}th node is not exist", pinned);
@@ -28,6 +36,7 @@ impl Rope {
         let mut res = Self {
             masses: vec![],
             springs: vec![],
+            masses_position: vec![],
         };
 
         // first mass
@@ -51,7 +60,90 @@ impl Rope {
         res
     }
 
-    pub fn simulate_verlet(&mut self, delta_t: u32, gravity: Vec2) {}
+    pub fn simulate_verlet(&mut self, delta_t: f32, gravity: Vec2) {}
 
-    pub fn simulate_euler(&mut self, delta_t: u32, gravity: Vec2) {}
+    // because of rust's super strong borrow check,
+    // we can't modify a variable who have multiple reference,
+    // thougth we know it's safety in single thread.
+    //
+    // so we use unsafe rust here and modify our mass fields by
+    // casting an Rc to a mutable reference as follow:
+    // ```
+    // deref_mass(&m).position = Vec2::ZERO;
+    // ```
+    pub fn simulate_euler(&mut self, delta_t: f32, gravity: Vec2) {
+        unsafe {
+            let deref_mass = |m: &Rc<Mass>| -> &mut Mass {
+                return (Rc::as_ptr(m) as *mut Mass).as_mut().unwrap();
+            };
+
+            for s in &self.springs {
+                // TODO (Part 2): Use Hooke's law to calculate the force on a node
+
+                let distence = s.m2.position - s.m1.position;
+                // f represent force apply to m1
+                let f = s.k * distence.normalize() * (distence.length() - s.rest_length);
+                deref_mass(&s.m1).forces += f;
+                deref_mass(&s.m2).forces += -f;
+            }
+
+            for m in &mut self.masses {
+                let mut m = deref_mass(m);
+                if !m.pinned {
+                    // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
+                    // TODO (Part 2): Add global damping
+                    m.forces += gravity;
+                    m.velocity *= f32::exp(-delta_t * 0.25);
+
+                    let a = m.forces / m.mass;
+                    let v_t1 = m.velocity + a * delta_t;
+                    // let x_t1 = m.position + m.velocity * delta_t; // for explicit method
+                    let x_t1 = m.position + v_t1 * delta_t; // for semi-implicit method
+
+                    m.velocity = v_t1;
+                    m.position = x_t1;
+                }
+
+                // Reset all forces on each mass
+                m.forces = Vec2::ZERO;
+            }
+        }
+    }
+
+    pub fn masses_positions(&self) -> impl Iterator<Item = Vec2> + '_ {
+        self.masses.iter().map(|x| x.position)
+    }
+
+    // return [(start_posi, end_posi)]
+    pub fn springs_position(&self) -> SpringsPosition {
+        SpringsPosition::new(&self.springs)
+    }
+}
+
+pub struct SpringsPosition<'a> {
+    springs: &'a Vec<Spring>,
+    index: usize,
+}
+
+impl Iterator for SpringsPosition<'_> {
+    type Item = (Vec2, Vec2);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.springs.len() {
+            let res = Some((
+                self.springs[self.index].m1.position,
+                self.springs[self.index].m2.position,
+            ));
+            self.index += 1;
+            return res;
+        }
+
+        return None;
+    }
+}
+
+impl<'a> SpringsPosition<'a> {
+    fn new(springs: &'a Vec<Spring>) -> Self {
+        Self { springs, index: 0 }
+    }
 }
